11dfadc0bffc918b33ff93132405cbe0ffb8045a
Safal Neupane <safal.npane@gmail.com>
2026-09-07 15:57:45 +1000

    fix: colour switch-and-apply flow

--- lua/apple/init.lua
+++ lua/apple/init.lua
@@ -3,32 +3,30 @@ local themeSwitch = require("apple.theme-switch")
 
 local M = {}
 
-function M.setup()
-  if vim.fn.exists("syntax_on") then
-    vim.cmd("syntax reset")
-  end
-  vim.cmd("highlight clear")
-
-  vim.o.termguicolors = true
-  vim.g.colors_name = "apple"
-
-  local colors = {}
-  themeSwitch.setup(function(mode)
-    if mode == "dark" then
-      vim.o.background = "dark"
-      colors = palette.dark
-    else
-      vim.o.background = "light"
-      colors = palette.light
-    end
-  end)
-
+local function apply_theme(colors)
   require("apple.highlights").setup(colors)
   require("apple.integrations.treesitter").setup(colors)
   require("apple.integrations.lsp").setup(colors)
   require("apple.integrations.telescope").setup(colors)
   require("apple.integrations.cmp").setup(colors)
   require("apple.integrations.gitsigns").setup(colors)
+end
+
+function M.setup()
+  vim.o.termguicolors = true
+  vim.g.colors_name = "apple"
+
+  -- Initial load
+  local is_dark = vim.o.background == "dark"
+  local initial_colors = is_dark and palette.dark or palette.light
+  apply_theme(initial_colors)
+
+  -- Listen for system changes
+  themeSwitch.setup(function(mode)
+    vim.o.background = mode
+    local colors = (mode == "dark") and palette.dark or palette.light
+    apply_theme(initial_colors)
+  end)
 end
 
 return M
